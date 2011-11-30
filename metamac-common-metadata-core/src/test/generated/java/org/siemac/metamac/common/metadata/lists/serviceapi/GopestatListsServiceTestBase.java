12ed4897d3a5b81197d50d1f97eaccb09812559c
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2011-11-30 14:55:16 +0000

    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@31902 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-common-metadata-core/src/test/generated/java/org/siemac/metamac/common/metadata/lists/serviceapi/GopestatListsServiceTestBase.java
@@ -0,0 +1,27 @@
+package org.siemac.metamac.gopestat.lists.serviceapi;
+
+
+/**
+ * Definition of test methods to implement.
+ */
+public interface GopestatListsServiceTestBase {
+    public void testFindOperationTypeById() throws Exception;
+
+    public void testFindAllOperationTypes() throws Exception;
+
+    public void testFindActivityClassById() throws Exception;
+
+    public void testFindAllActivityClasses() throws Exception;
+
+    public void testFindActivityTypeById() throws Exception;
+
+    public void testFindAllActivityTypes() throws Exception;
+
+    public void testFindSourceDataById() throws Exception;
+
+    public void testFindAllSourcesData() throws Exception;
+
+    public void testFindOfficialityTypeById() throws Exception;
+
+    public void testFindAllOfficialityTypes() throws Exception;
+}
