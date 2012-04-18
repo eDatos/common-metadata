915207e5b530084ade163b5c91e977b4aa2b7b96
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-18 19:44:01 +0000

    Eliminado código comentado
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@35144 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/mapper/Date2JodaTimeConverter.java
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/mapper/Date2JodaTimeConverter.java
@@ -21,21 +21,8 @@ public class Date2JodaTimeConverter implements CustomConverter {
             return new DateTime(source);
         } else if (source instanceof DateTime) {
 
-            // Calendar calendar = ((AbstractDateTime) source).toCalendar(Locale.getDefault());
-            // Date date = ((AbstractInstant) source).toDate();
-
-            // return date;
             return ((DateTime) source).toDate();
 
-            // Calendar result;
-            // if (destination == null) {
-            // result = Calendar.getInstance();
-            // }
-            // else {
-            // result = (Calendar) destination;
-            // }
-            // result.setTimeInMillis(((DateTime) source).getMillis());
-            // return result;
         } else {
             throw new MappingException("Converter Date2JodaTimeConverter used incorrectly. Arguments passed in were:" + destination + " and " + source);
         }
