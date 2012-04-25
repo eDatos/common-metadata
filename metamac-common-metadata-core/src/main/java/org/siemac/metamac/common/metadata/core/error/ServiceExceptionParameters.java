1ee7449f0901529b148186151e969af6c744f5b1
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-04-25 08:08:02 +0000

    Incluídas validaciones de URL
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@35425 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/error/ServiceExceptionParameters.java
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/error/ServiceExceptionParameters.java
@@ -2,16 +2,20 @@ package org.siemac.metamac.common.metadata.core.error;
 
 public class ServiceExceptionParameters {
 
-    public static final String ID                                = "id";
+    public static final String ID                                    = "id";
 
-    public static final String CONFIGURATION                     = "configuration";
+    public static final String CONFIGURATION                         = "configuration";
 
-    public static final String CONFIGURATION_ID                  = "configuration.id";
-    public static final String CONFIGURATION_UUID                = "configuration.uuid";
-    public static final String CONFIGURATION_CODE                = "configuration.code";
-    public static final String CONFIGURATION_LEGAL_ACTS          = "configuration.legal_acts";
-    public static final String CONFIGURATION_DATA_SHARING        = "configuration.data_sharing";
-    public static final String CONFIGURATION_CONF_POLICY         = "configuration.conf_policy";
-    public static final String CONFIGURATION_CONF_DATA_TREATMENT = "configuration.data_treatment";
+    public static final String CONFIGURATION_ID                      = "configuration.id";
+    public static final String CONFIGURATION_UUID                    = "configuration.uuid";
+    public static final String CONFIGURATION_CODE                    = "configuration.code";
+    public static final String CONFIGURATION_LEGAL_ACTS              = "configuration.legal_acts";
+    public static final String CONFIGURATION_DATA_SHARING            = "configuration.data_sharing";
+    public static final String CONFIGURATION_CONF_POLICY             = "configuration.conf_policy";
+    public static final String CONFIGURATION_CONF_DATA_TREATMENT     = "configuration.data_treatment";
+    public static final String CONFIGURATION_CONF_DATA_TREATMENT_URL = "configuration.conf_data_treatment_url";
+    public static final String CONFIGURATION_CONF_POLICY_URL         = "configuration.conf_policy_url";
+    public static final String CONFIGURATION_DATA_SHARING_URL        = "configuration.data_sharing_url";
+    public static final String CONFIGURATION_LEGAL_ACTS_URL          = "configuration.legal_acts_url";
 
 }
--- metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/serviceimpl/utils/InvocationValidator.java
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/serviceimpl/utils/InvocationValidator.java
@@ -83,7 +83,6 @@ public class InvocationValidator {
 
     }
 
-    
     // ------------------------------------------------------------------------------------
     // PRIVATE METHODS
     // ------------------------------------------------------------------------------------
@@ -92,7 +91,11 @@ public class InvocationValidator {
         ValidationUtils.checkMetadataRequired(configuration.getCode(), ServiceExceptionParameters.CONFIGURATION_CODE, exceptions);
         if (configuration.getCode() != null && !CoreCommonUtil.isSemanticIdentifier(configuration.getCode())) {
             exceptions.add(new MetamacExceptionItem(ServiceExceptionType.METADATA_INCORRECT, ServiceExceptionParameters.CONFIGURATION_CODE));
-            }
+        }
+        ValidationUtils.validateUrl(configuration.getConfDataTreatmentUrl(), ServiceExceptionParameters.CONFIGURATION_CONF_DATA_TREATMENT_URL, exceptions);
+        ValidationUtils.validateUrl(configuration.getConfPolicyUrl(), ServiceExceptionParameters.CONFIGURATION_CONF_POLICY_URL, exceptions);
+        ValidationUtils.validateUrl(configuration.getDataSharingUrl(), ServiceExceptionParameters.CONFIGURATION_DATA_SHARING_URL, exceptions);
+        ValidationUtils.validateUrl(configuration.getLegalActsUrl(), ServiceExceptionParameters.CONFIGURATION_LEGAL_ACTS_URL, exceptions);
 
     }
 }
