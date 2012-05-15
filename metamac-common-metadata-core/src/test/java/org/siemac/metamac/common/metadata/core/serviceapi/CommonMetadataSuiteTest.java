1daa3e833bece6f820db0ce881a90fc1d61e5074
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-05-15 12:39:13 +0000

    METAMAC-545
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@35979 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-common-metadata-core/src/test/java/org/siemac/metamac/common/metadata/core/serviceapi/CommonMetadataSuiteTest.java
@@ -0,0 +1,13 @@
+package org.siemac.metamac.common.metadata.core.serviceapi;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ * Spring based transactional test with DbUnit support.
+ */
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CommonMetadataServiceFacadeTest.class, CommonMetadataServiceTest.class, SecurityCommonMetadataServiceFacadeTest.class})
+public class CommonMetadataSuiteTest {
+}
