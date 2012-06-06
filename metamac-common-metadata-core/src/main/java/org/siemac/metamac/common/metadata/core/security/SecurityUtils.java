34f79686d55550a9080ab2d33dddc030b02eac84
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-06-06 07:25:31 +0000

    Seguridad en la web
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@37251 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/security/SecurityUtils.java
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/security/SecurityUtils.java
@@ -1,15 +1,14 @@
 package org.siemac.metamac.common.metadata.core.security;
 
 import org.fornax.cartridges.sculptor.framework.errorhandling.ServiceContext;
-import org.siemac.metamac.common.metadata.core.constants.CommonMetadataConstants;
 import org.siemac.metamac.common.metadata.core.error.ServiceExceptionType;
+import org.siemac.metamac.common.metadata.core.security.shared.SharedSecurityUtils;
 import org.siemac.metamac.core.common.exception.MetamacException;
 import org.siemac.metamac.domain.common.metadata.enume.domain.CommonMetadataRoleEnum;
 import org.siemac.metamac.sso.client.MetamacPrincipal;
-import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
 import org.siemac.metamac.sso.client.SsoClientConstants;
 
-public class SecurityUtils {
+public class SecurityUtils extends SharedSecurityUtils {
 
     /**
      * Checks user can execute any operation, if has any role of requested roles
@@ -35,18 +34,6 @@ public class SecurityUtils {
     }
 
     /**
-     * Checks user has requested rol
-     */
-    private static boolean isUserInRol(MetamacPrincipal metamacPrincipal, CommonMetadataRoleEnum role) throws MetamacException {
-
-        if (CommonMetadataRoleEnum.ANY_ROLE_ALLOWED.equals(role)) {
-            return isAnyCommonMetadataRole(metamacPrincipal);
-        } else {
-            return isRoleInAccesses(metamacPrincipal, role);
-        }
-    }
-
-    /**
      * Retrieves MetamacPrincipal in ServiceContext
      */
     private static MetamacPrincipal getMetamacPrincipal(ServiceContext ctx) throws MetamacException {
@@ -59,31 +46,6 @@ public class SecurityUtils {
             throw new MetamacException(ServiceExceptionType.SECURITY_PRINCIPAL_NOT_FOUND);
         }
         return metamacPrincipal;
-    }
-
-    /**
-     * Checks if user has access with role
-     */
-    private static Boolean isRoleInAccesses(MetamacPrincipal metamacPrincipal, CommonMetadataRoleEnum role) {
-        for (MetamacPrincipalAccess metamacPrincipalAccess : metamacPrincipal.getAccesses()) {
-            if (CommonMetadataConstants.SECURITY_APPLICATION_ID.equals(metamacPrincipalAccess.getApplication()) && metamacPrincipalAccess.getRole().equals(role.name())) {
-                return Boolean.TRUE;
-            }
-        }
-        return Boolean.FALSE;
-    }
-
-    private static Boolean isAnyCommonMetadataRole(MetamacPrincipal metamacPrincipal) {
-        return isAdministrador(metamacPrincipal) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_PLANIFICACION)
-                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.JEFE_NORMALIZACION) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_APOYO_DIFUSION)
-                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_APOYO_NORMALIZACION) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_APOYO_PLANIFICACION)
-                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_APOYO_PRODUCCION) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_DIFUSION)
-                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_NORMALIZACION) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_PLANIFICACION)
-                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_PRODUCCION);
-    }
-
-    private static Boolean isAdministrador(MetamacPrincipal metamacPrincipal) {
-        return isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.ADMINISTRADOR);
     }
 
 }
---
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/security/shared/SharedSecurityUtils.java
@@ -0,0 +1,47 @@
+package org.siemac.metamac.common.metadata.core.security.shared;
+
+import org.siemac.metamac.common.metadata.core.constants.CommonMetadataConstants;
+import org.siemac.metamac.domain.common.metadata.enume.domain.CommonMetadataRoleEnum;
+import org.siemac.metamac.sso.client.MetamacPrincipal;
+import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
+
+public class SharedSecurityUtils {
+
+    /**
+     * Checks user has requested role
+     */
+    public static boolean isUserInRol(MetamacPrincipal metamacPrincipal, CommonMetadataRoleEnum role) {
+
+        if (CommonMetadataRoleEnum.ANY_ROLE_ALLOWED.equals(role)) {
+            return isAnyCommonMetadataRole(metamacPrincipal);
+        } else {
+            return isRoleInAccesses(metamacPrincipal, role);
+        }
+    }
+
+    /**
+     * Checks if user has access with role
+     */
+    public static Boolean isRoleInAccesses(MetamacPrincipal metamacPrincipal, CommonMetadataRoleEnum role) {
+        for (MetamacPrincipalAccess metamacPrincipalAccess : metamacPrincipal.getAccesses()) {
+            if (CommonMetadataConstants.SECURITY_APPLICATION_ID.equals(metamacPrincipalAccess.getApplication()) && metamacPrincipalAccess.getRole().equals(role.name())) {
+                return Boolean.TRUE;
+            }
+        }
+        return Boolean.FALSE;
+    }
+
+    public static Boolean isAnyCommonMetadataRole(MetamacPrincipal metamacPrincipal) {
+        return isAdministrador(metamacPrincipal) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_PLANIFICACION)
+                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.JEFE_NORMALIZACION) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_APOYO_DIFUSION)
+                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_APOYO_NORMALIZACION) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_APOYO_PLANIFICACION)
+                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_APOYO_PRODUCCION) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_DIFUSION)
+                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_NORMALIZACION) || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_PLANIFICACION)
+                || isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.TECNICO_PRODUCCION);
+    }
+
+    public static Boolean isAdministrador(MetamacPrincipal metamacPrincipal) {
+        return isRoleInAccesses(metamacPrincipal, CommonMetadataRoleEnum.ADMINISTRADOR);
+    }
+
+}
