300fc5493f6a4acb962cb014d527fc6a367a7819
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-06 09:42:19 +0000

    in progress: METAMAC-777 Crear API Rest de Common metadata. Creando proyecto rest-internal-impl
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@38508 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-common-metadata-rest-internal-impl/src/main/java/org/siemac/metamac/common/metadata/rest/internal/RestInternalConstants.java
@@ -0,0 +1,12 @@
+package org.siemac.metamac.common.metadata.rest.internal;
+
+public class RestInternalConstants {
+
+    public static String  LINK_SUBPATH_CONFIGURATIONS = "configurations";
+
+    private static String API_NAME                    = "commonMetadata";
+
+    private static String KIND_SEPARATOR              = "#";
+    public static String  KIND_CONFIGURATIONS         = API_NAME + KIND_SEPARATOR + "configurations";
+    public static String  KIND_CONFIGURATION          = API_NAME + KIND_SEPARATOR + "configuration";
+}
---
+++ metamac-common-metadata-rest-internal-impl/src/main/java/org/siemac/metamac/common/metadata/rest/internal/exception/RestServiceExceptionType.java
@@ -0,0 +1,8 @@
+package org.siemac.metamac.common.metadata.rest.internal.exception;
+
+import org.siemac.metamac.rest.exception.RestCommonServiceExceptionType;
+
+public class RestServiceExceptionType extends RestCommonServiceExceptionType {
+
+    public static final RestCommonServiceExceptionType CONFIGURATION_NOT_FOUND = create("exception.common_metadata.configuration.not_found");
+}
