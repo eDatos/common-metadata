5c62f25008e12cb3733bbbf405199de7b261ac85
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2012-07-18 13:43:21 +0000

    Refactor RestConstants a paquete de constantes. Añadido "kind" a constantes
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@38909 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-rest-internal-impl/src/main/java/org/siemac/metamac/common/metadata/rest/internal/RestInternalConstants.java
+++ metamac-common-metadata-rest-internal-impl/src/main/java/org/siemac/metamac/common/metadata/rest/internal/RestInternalConstants.java
@@ -1,12 +1,13 @@
 package org.siemac.metamac.common.metadata.rest.internal;
 
+import org.siemac.metamac.rest.constants.RestConstants;
+
 public class RestInternalConstants {
 
     public static String  LINK_SUBPATH_CONFIGURATIONS = "configurations";
 
     private static String API_NAME                    = "commonMetadata";
 
-    private static String KIND_SEPARATOR              = "#";
-    public static String  KIND_CONFIGURATIONS         = API_NAME + KIND_SEPARATOR + "configurations";
-    public static String  KIND_CONFIGURATION          = API_NAME + KIND_SEPARATOR + "configuration";
+    public static String  KIND_CONFIGURATIONS         = API_NAME + RestConstants.KIND_SEPARATOR + "configurations";
+    public static String  KIND_CONFIGURATION          = API_NAME + RestConstants.KIND_SEPARATOR + "configuration";
 }
