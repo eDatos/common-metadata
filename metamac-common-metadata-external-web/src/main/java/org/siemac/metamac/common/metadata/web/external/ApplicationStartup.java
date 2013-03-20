194ea495bcfc18986a29e6c56b29bd815fc8c32a
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-03-20 09:53:13 +0000

    METAMAC-1564: Configuración y control de propiedades del DATA para aplicaciones internas y externas
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@44496 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ metamac-common-metadata-external-web/src/main/java/org/siemac/metamac/common/metadata/web/external/ApplicationStartup.java
@@ -0,0 +1,80 @@
+package org.siemac.metamac.common.metadata.web.external;
+
+import javax.servlet.ServletContextEvent;
+import javax.servlet.ServletContextListener;
+
+import org.apache.commons.lang.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.siemac.metamac.common.metadata.core.constants.CommonMetadataConstants;
+import org.siemac.metamac.core.common.conf.ConfigurationService;
+import org.siemac.metamac.core.common.constants.CoreCommonConstants;
+import org.siemac.metamac.core.common.util.ApplicationContextProvider;
+import org.siemac.metamac.rest.constants.RestEndpointsConstants;
+
+public class ApplicationStartup implements ServletContextListener {
+
+    private static final Log     LOG = LogFactory.getLog(ApplicationStartup.class);
+
+    private ConfigurationService configurationService;
+
+    @Override
+    public void contextInitialized(ServletContextEvent sce) {
+        try {
+            configurationService = ApplicationContextProvider.getApplicationContext().getBean(ConfigurationService.class);
+            checkConfiguration();
+        } catch (Exception e) {
+            // Abort startup application
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void checkConfiguration() {
+        LOG.info("**********************************************************");
+        LOG.info("Checking application configuration");
+        LOG.info("**********************************************************");
+
+        // Datasource
+        checkProperty(CommonMetadataConstants.DB_DRIVER_NAME);
+        checkProperty(CommonMetadataConstants.DB_URL);
+        checkProperty(CommonMetadataConstants.DB_USERNAME);
+        checkProperty(CommonMetadataConstants.DB_PASSWORD);
+        checkProperty(CommonMetadataConstants.DB_DIALECT);
+
+        // Api
+        checkProperty(RestEndpointsConstants.COMMON_METADATA_EXTERNAL_API);
+        checkProperty(RestEndpointsConstants.SRM_EXTERNAL_API);
+
+        LOG.info("**********************************************************");
+        LOG.info("Application configuration checked");
+        LOG.info("**********************************************************");
+    }
+
+    private void checkProperty(String propertyKey) {
+        checkProperty(propertyKey, true);
+    }
+
+    private void checkProperty(String propertyKey, boolean required) {
+        String propertyValue = configurationService.getProperty(propertyKey);
+
+        // Check filled
+        if (required && StringUtils.isBlank(propertyValue)) {
+            String errorMessage = "Property [" + propertyKey + "] is required and it is not setted. Aborting application startup";
+            LOG.error(errorMessage);
+            throw new IllegalArgumentException(errorMessage);
+        }
+
+        // Check correctly filled
+        if (!StringUtils.isBlank(propertyValue) && propertyValue.contains(CoreCommonConstants.CONFIGURATION_FILL_ME_PROPERTY)) {
+            String errorMessage = "Property [" + propertyKey + "] is not correctly filled. Aborting application startup";
+            LOG.error(errorMessage);
+            throw new IllegalArgumentException(errorMessage);
+        }
+        LOG.info("Property [" + propertyKey + "] filled"); // NOTE: Client requirement: Do not log properties values
+    }
+
+    @Override
+    public void contextDestroyed(ServletContextEvent sce) {
+    }
+
+}
