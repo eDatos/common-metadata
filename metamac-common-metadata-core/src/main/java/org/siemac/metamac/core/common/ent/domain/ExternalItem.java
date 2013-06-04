20ef818b1981618181200f528973e7b5140bd10b
smarrui <smarrui@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-04 11:05:13 +0000

    METAMAC-1695: External Item con URN opcional
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@46351 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-core/src/main/java/org/siemac/metamac/core/common/ent/domain/ExternalItem.java
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/core/common/ent/domain/ExternalItem.java
@@ -1,9 +1,9 @@
 package org.siemac.metamac.core.common.ent.domain;
 
-import org.siemac.metamac.core.common.enume.domain.TypeExternalArtefactsEnum;
-
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
+import org.siemac.metamac.core.common.enume.domain.TypeExternalArtefactsEnum;
 
 /**
  * Entity for store information about ExternalItems.
@@ -11,19 +11,21 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "TB_EXTERNAL_ITEMS")
 public class ExternalItem extends ExternalItemBase {
+
     private static final long serialVersionUID = 1L;
 
     protected ExternalItem() {
     }
 
-    public ExternalItem(String code, String uri, String urn, TypeExternalArtefactsEnum type) { 
-        super(code, uri, urn, type); 
-    } 
-     
-     
-    public ExternalItem(String code, String uri, String urn, TypeExternalArtefactsEnum type, InternationalString title, String managementAppUrl) { 
-        super(code, uri, urn, type); 
-        this.setTitle(title); 
-        this.setManagementAppUrl(managementAppUrl); 
-    } 
+    public ExternalItem(String code, String uri, String urn, TypeExternalArtefactsEnum type) {
+        super(code, uri, type);
+        this.setUrn(urn);
+    }
+
+    public ExternalItem(String code, String uri, String urn, TypeExternalArtefactsEnum type, InternationalString title, String managementAppUrl) {
+        super(code, uri, type);
+        this.setUrn(urn);
+        this.setTitle(title);
+        this.setManagementAppUrl(managementAppUrl);
+    }
 }
