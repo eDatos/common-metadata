4bcf708d142fde079c70ee45f9510195214e10de
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-06-11 08:11:42 +0000

    METAMAC-1719 - [Excepciones]: traducción de excepciones en el CORE. Intersector de traduccion de mensajes.
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@46675 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/error/ServiceExceptionParameters.java
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/core/error/ServiceExceptionParameters.java
@@ -1,23 +1,23 @@
 package org.siemac.metamac.common.metadata.core.error;
 
-public class ServiceExceptionParameters {
+import org.siemac.metamac.core.common.exception.CommonServiceExceptionParameters;
 
-    public static final String ID                                = "id";
-    public static final String URN                                = "urn";
-    public static final String STATUS                            = "status";
-    public static final String CONFIGURATION_IDS                 = "configurationIds";
+public class ServiceExceptionParameters extends CommonServiceExceptionParameters {
 
-    public static final String CONFIGURATION                     = "configuration";
+    public static final String STATUS                            = "parameter.common_metadata.status";
+    public static final String CONFIGURATION_IDS                 = "parameter.common_metadata.configurationIds";
 
-    public static final String CONFIGURATION_ID                  = "configuration.id";
-    public static final String CONFIGURATION_UUID                = "configuration.uuid";
-    public static final String CONFIGURATION_CODE                = "configuration.code";
-    public static final String CONFIGURATION_URN                 = "configuration.urn";
-    public static final String CONFIGURATION_LEGAL_ACTS          = "configuration.legal_acts";
-    public static final String CONFIGURATION_DATA_SHARING        = "configuration.data_sharing";
-    public static final String CONFIGURATION_CONF_POLICY         = "configuration.conf_policy";
-    public static final String CONFIGURATION_CONF_DATA_TREATMENT = "configuration.data_treatment";
-    public static final String CONFIGURATION_CONTACT             = "configuration.contact";
+    public static final String CONFIGURATION                     = "parameter.common_metadata.configuration";
 
-    public static final String CONFIGURATION_STATUS              = "configuration.status";
+    public static final String CONFIGURATION_ID                  = "parameter.common_metadata.configuration.id";
+    public static final String CONFIGURATION_UUID                = "parameter.common_metadata.configuration.uuid";
+    public static final String CONFIGURATION_CODE                = "parameter.common_metadata.configuration.code";
+    public static final String CONFIGURATION_URN                 = "parameter.common_metadata.configuration.urn";
+    public static final String CONFIGURATION_LEGAL_ACTS          = "parameter.common_metadata.configuration.legal_acts";
+    public static final String CONFIGURATION_DATA_SHARING        = "parameter.common_metadata.configuration.data_sharing";
+    public static final String CONFIGURATION_CONF_POLICY         = "parameter.common_metadata.configuration.conf_policy";
+    public static final String CONFIGURATION_CONF_DATA_TREATMENT = "parameter.common_metadata.configuration.data_treatment";
+    public static final String CONFIGURATION_CONTACT             = "parameter.common_metadata.configuration.contact";
+
+    public static final String CONFIGURATION_STATUS              = "parameter.common_metadata.configuration.status";
 }
