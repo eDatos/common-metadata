6cb044d14bb667652077756f1754c9a60dd08bea
zmencor <zmencor@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-11-13 10:02:21 +0000

    METAMAC-1969: Configuraciones de aplicaciones en Base de datos en vez de en archivo de propiedades http://jira.arte-consultores.com/browse/METAMAC-1969
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@49966 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/db/mssql/03-insert/01-configurations_insert_example.sql
+++ etc/db/mssql/03-insert/01-configurations_insert_example.sql
@@ -2,10 +2,10 @@
 -- # Insert
 -- ###########################################
 
-insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.db.url','jdbc:oracle:thin:@FILL_ME_WITH_HOST:FILL_ME_WITH_PORT:XE');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.db.url','jdbc:sqlserver://FILL_ME_WITH_HOST:FILL_ME_WITH_PORT:XE;databaseName=FILL_ME_WITH_DATABASE_NAME');
 insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.db.username','FILL_ME_WITH_USERNAME');
 insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.db.password','FILL_ME_WITH_PASSWORD');
-insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.db.dialect','org.siemac.metamac.hibernate.dialect.Oracle10gDialectMetamac');
-insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.db.driver_name','oracle.jdbc.OracleDriver');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.db.dialect','org.hibernate.dialect.SQLServerDialect');
+insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.db.driver_name','org.hibernate.dialect.SQLServerDialect');
 
 insert into TB_CONFIGURATIONS (CONF_KEY,CONF_VALUE) values ('metamac.common_metadata.user_guide.file_name','Gestor_metadatos_comunes-Manual_usuario.pdf');
