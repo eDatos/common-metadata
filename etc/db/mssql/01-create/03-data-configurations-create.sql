ba5c30bb223e501670d760adb1304a138691591d
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-12-09 09:08:47 +0000

    METAMAC-1997 - Las configuraciones en base de datos se guardarán en el common metadata
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@50225 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ etc/db/mssql/01-create/03-data-configurations-create.sql
@@ -0,0 +1,23 @@
+-- ###########################################
+-- # Create
+-- ###########################################
+-- Create pk sequence
+     
+-- Create normal entities
+    
+CREATE TABLE TB_DATA_CONFIGURATIONS (
+  CONF_KEY   NVARCHAR(255) NOT NULL PRIMARY KEY,
+  CONF_VALUE NVARCHAR(4000)
+);
+
+-- Create many to many relations
+    
+-- Primary keys
+
+-- Unique constraints
+     
+-- Foreign key constraints
+   
+-- Index
+
+
---
+++ etc/db/mssql/02-drop/03-data-configurations-drop.sql
@@ -0,0 +1,11 @@
+-- ###########################################
+-- # Drop
+-- ###########################################
+-- Drop index
+
+-- Drop many to many relations
+    
+-- Drop normal entities
+DROP TABLE TB_DATA_CONFIGURATIONS CASCADE CONSTRAINTS PURGE;
+
+-- Drop sequences
