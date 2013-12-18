9cdc4c9acdcdfe7e4a9c73dfe6d78a4d569f83d2
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2013-12-18 14:07:38 +0000

    METAMAC-2006 - [CORE] Refactor de la constante SECURITY_APPLICATION_ID por APPLICATION_ID
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@50388 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-web/src/main/java/org/siemac/metamac/common/metadata/web/client/LoggedInGatekeeper.java
+++ metamac-common-metadata-web/src/main/java/org/siemac/metamac/common/metadata/web/client/LoggedInGatekeeper.java
@@ -10,8 +10,8 @@ import org.siemac.metamac.sso.client.MetamacPrincipalAccess;
 import org.siemac.metamac.web.common.client.events.LoginAuthenticatedEvent;
 import org.siemac.metamac.web.common.client.events.LoginAuthenticatedEvent.LoginAuthenticatedEventHandler;
 
-import com.google.web.bindery.event.shared.EventBus;
 import com.google.inject.Inject;
+import com.google.web.bindery.event.shared.EventBus;
 import com.gwtplatform.mvp.client.proxy.Gatekeeper;
 
 public class LoggedInGatekeeper implements Gatekeeper {
@@ -36,7 +36,7 @@ public class LoggedInGatekeeper implements Gatekeeper {
 
     private boolean hasAnyAllowedRole(MetamacPrincipal metamacPrincipal) {
         for (MetamacPrincipalAccess access : metamacPrincipal.getAccesses()) {
-            if (CommonMetadataConstants.SECURITY_APPLICATION_ID.equals(access.getApplication()) && isRoleAllowed(access.getRole())) {
+            if (CommonMetadataConstants.APPLICATION_ID.equals(access.getApplication()) && isRoleAllowed(access.getRole())) {
                 return true;
             }
         }
