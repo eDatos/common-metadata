ee537dca5ce60455a2d4162618cb1884ac805244
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-01-28 09:33:16 +0000

    METAMAC-2094 - Añadir commit a los scripts de actualización desde la versión 1.4.0
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@50880 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-from-release/1.4.0/db/20140115.sql
+++ etc/changes-from-release/1.4.0/db/20140115.sql
@@ -8,3 +8,5 @@ update TB_DATA_CONFIGURATIONS set CONF_VALUE = '${metamac.portal.rest.external.b
 
 insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values(GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.portal.rest.external.permalinks', '${metamac.portal.rest.external}/permalinks');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+commit;
