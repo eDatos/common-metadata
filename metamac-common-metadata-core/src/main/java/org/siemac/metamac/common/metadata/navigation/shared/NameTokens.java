12a957496756c736ea0bac5264eb9b641b1cc5e2
rcorrod <rcorrod@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-02-26 10:24:39 +0000

    METAMAC-1976 - Posibilidad de setear en la WEB los datos por defecto de data Seguridad añadida
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@51104 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/navigation/shared/NameTokens.java
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/navigation/shared/NameTokens.java
@@ -2,11 +2,14 @@ package org.siemac.metamac.common.metadata.navigation.shared;
 
 public class NameTokens {
 
-    public static final String mainPage               = "main";
-    public static final String configurationListPage  = "configurations";
-    public static final String configurationPage      = "configuration";
+    public static final String mainPage                 = "main";
+    public static final String commonMetadataListPage   = "configurations";
+    public static final String commonMetadataPage       = "configuration";
 
-    public static final String errorPage              = "error";
-    public static final String unauthorizedAccessPage = "unauthorizedAccess";
+    public static final String systemPropertiesListPage = "systemProperties";
+    public static final String defaultValuesListPage    = "defaultValues";
+
+    public static final String errorPage                = "error";
+    public static final String unauthorizedAccessPage   = "unauthorizedAccess";
 
 }
--- metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/navigation/shared/PlaceRequestParams.java
+++ metamac-common-metadata-core/src/main/java/org/siemac/metamac/common/metadata/navigation/shared/PlaceRequestParams.java
@@ -2,5 +2,5 @@ package org.siemac.metamac.common.metadata.navigation.shared;
 
 public class PlaceRequestParams {
 
-    public static final String configurationParamId = "id";
+    public static final String commonMetadataParamId = "id";
 }
