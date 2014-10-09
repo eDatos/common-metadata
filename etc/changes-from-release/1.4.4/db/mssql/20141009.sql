8cf45622d8fbb6797ea33ad3c2194502ee6e552d
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-10-09 07:27:25 +0000

    METAMAC-2052 - Modificar plantillas de e-mails para que se correspondan con las definidas por el cliente >> Enviar URL de aviso en el email
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@53437 05f23f4b-1f0b-0410-ba1b-ad9509270e59

---
+++ etc/changes-from-release/1.4.4/db/mssql/20141009.sql
@@ -0,0 +1,6 @@
+-- -------------------------------------------------------------------------------------------------------------------
+--METAMAC-2052 - Añadir propiedad de URL de aplicación interna para los envíos de emails de avisos.
+-- -------------------------------------------------------------------------------------------------------------------
+
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'),1,1,'metamac.notices.web.internal','http://FILL_ME_WITH_HOST_AND_PORT/FILL_ME_WITH_NOTICES_INTERNAL_APP');
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
