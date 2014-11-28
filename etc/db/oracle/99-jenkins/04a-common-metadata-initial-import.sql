6c6ee0be51752bb076b4e75123a8f766687c9428
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2014-11-28 08:21:57 +0000

    METAMAC-2376 - Actualización de scripts SQL y environment.xml de ORACLE para no especificar el SID por defecto
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@53663 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/db/oracle/99-jenkins/04a-common-metadata-initial-import.sql
+++ etc/db/oracle/99-jenkins/04a-common-metadata-initial-import.sql
@@ -2,7 +2,7 @@
 -- # Insert
 -- ###########################################
 
-insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.common_metadata.db.url','jdbc:oracle:thin:@oracle-dev:1521:XE');
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.common_metadata.db.url','jdbc:oracle:thin:@oracle-dev:1521:FILL_ME_WITH_ORACLE_SID');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
 insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (GET_NEXT_SEQUENCE_VALUE('DATA_CONFIGURATIONS'),1,1,'metamac.common_metadata.db.username','jenkins_metamac_common');
