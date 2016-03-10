48288acd75c514955e4b5e0e631c438a25627e62
rdiaada <rdiaada@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-03-10 08:50:58 +0000

    NONE - Formato
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@54989 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- etc/changes-from-release/1.4.6/db/mssql/20160304_insert-help-url-data-configuration.sql
+++ etc/changes-from-release/1.4.6/db/mssql/20160304_insert-help-url-data-configuration.sql
@@ -3,12 +3,10 @@
 -- --------------------------------------------------------------------------------------------------
 
 -- Añadir propiedad con la URL de la ayuda
-
 insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE) values (FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'),1,1,'metamac.common_metadata.help.url','FILL_ME_WITH_HELP_URL');
 UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
 
 
 -- Eliminar propiedad con el nombre del manual de usuario
-
 delete from TB_DATA_CONFIGURATIONS
 where CONF_KEY = 'metamac.common_metadata.user_guide.file_name';
