56172059e1afe70c6db22d2d7689870f095cb7cd
lgutmed <lgutmed@05f23f4b-1f0b-0410-ba1b-ad9509270e59>
2016-06-08 09:13:41 +0000

    METAMAC-2452 - Los archivos urlrewrite de las aplicaciones externas no mantienen el contexto de la aplicación
    > Common metadata
    > Statistical resources
    
    git-svn-id: http://svn.arte-consultores.com/svn/arte/istac/metamac/metamac-common-metadata/trunk/metamac-common-metadata@55407 05f23f4b-1f0b-0410-ba1b-ad9509270e59

--- metamac-common-metadata-external-web/src/main/java/org/siemac/metamac/common/metadata/web/external/urlrewrite/CommonMetadataRewriteMatch.java
+++ metamac-common-metadata-external-web/src/main/java/org/siemac/metamac/common/metadata/web/external/urlrewrite/CommonMetadataRewriteMatch.java
@@ -1,16 +1,40 @@
 package org.siemac.metamac.common.metadata.web.external.urlrewrite;
 
+import javax.servlet.ServletException;
+
 import org.siemac.metamac.common_metadata.rest.external.RestExternalConstants;
+import org.siemac.metamac.core.common.conf.ConfigurationService;
+import org.siemac.metamac.core.common.exception.MetamacException;
+import org.siemac.metamac.core.common.util.ApplicationContextProvider;
 import org.siemac.metamac.core.common.util.urlrewrite.AbstractRewriteMatch;
 
 class CommonMetadataRewriteMatch extends AbstractRewriteMatch {
+
+    private ConfigurationService configurationService = null;
 
     @Override
     protected String getApiPrefix() {
         return "cmetadata";
     }
+
     @Override
     protected String getLatestApiVersion() {
         return RestExternalConstants.API_VERSION_1_0;
+    }
+
+    @Override
+    protected String getApiBaseUrl() throws ServletException {
+        try {
+            return getConfigurationService().retrieveCommonMetadataExternalApiUrlBase();
+        } catch (MetamacException e) {
+            throw new ServletException("Error retrieving configuration property of the external API URL base", e);
+        }
+    }
+
+    private ConfigurationService getConfigurationService() {
+        if (configurationService == null) {
+            configurationService = ApplicationContextProvider.getApplicationContext().getBean(ConfigurationService.class);
+        }
+        return configurationService;
     }
 }
