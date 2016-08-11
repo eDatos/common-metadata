ffc8e84a71396b393467622b710d47128cfce67d
Zebensui Méndez Corujo <zmencor@arte-consultores.com>
2016-08-11 07:29:42 +0100

    METAMAC-2475 - Migración de las aplicaciones de Tomcat6 a Tomcat8 y JDK8
    - Deploy

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -1,17 +1,18 @@
 #!/bin/sh
 
-TMP_PATH=/servers/metamac/tmp
+HOME_PATH=metamac-common-metadata
+TRANSFER_PATH=$HOME_PATH/tmp
 DEPLOY_TARGET_PATH=/servers/metamac/tomcats/metamac01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
 
-scp -r etc/deploy deploy@estadisticas.arte-consultores.com:
-scp metamac-common-metadata-web/target/common-metadata-internal-*.war deploy@estadisticas.arte-consultores.com:common-metadata-internal.war
-scp metamac-common-metadata-external-web/target/common-metadata-*.war deploy@estadisticas.arte-consultores.com:common-metadata.war
+scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
+scp metamac-common-metadata-web/target/common-metadata-internal-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/common-metadata-internal.war
+scp metamac-common-metadata-external-web/target/common-metadata-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/common-metadata.war
 ssh deploy@estadisticas.arte-consultores.com <<EOF
 
-    chmod a+x deploy/*.sh;
-    . deploy/utilities.sh
+    chmod a+x $TRANSFER_PATH/deploy/*.sh;
+    . $TRANSFER_PATH/deploy/utilities.sh
     
     sudo service metamac01 stop
     checkPROC "metamac"
@@ -19,35 +20,31 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     ###
     # COMMON-METADATA-INTERNAL
     ###
-    # Backup Configuration
-    sudo mv $DEPLOY_TARGET_PATH/common-metadata-internal/$ENVIRONMENT_RELATIVE_PATH_FILE $TMP_PATH/environment.xml_common-metadata-internal_tmp
-    sudo mv $DEPLOY_TARGET_PATH/common-metadata-internal/$LOGBACK_RELATIVE_PATH_FILE $TMP_PATH/logback.xml_common-metadata-internal_tmp
     
     # Update Process
-    sudo mv common-metadata-internal.war $DEPLOY_TARGET_PATH/common-metadata-internal.war
-    sudo unzip $DEPLOY_TARGET_PATH/common-metadata-internal.war -d $DEPLOY_TARGET_PATH
+    sudo rm -rf $DEPLOY_TARGET_PATH/common-metadata-internal
+    sudo mv $TRANSFER_PATH/common-metadata-internal.war $DEPLOY_TARGET_PATH/common-metadata-internal.war
+    sudo unzip $DEPLOY_TARGET_PATH/common-metadata-internal.war -d $DEPLOY_TARGET_PATH/common-metadata-internal
     sudo rm -rf $DEPLOY_TARGET_PATH/common-metadata-internal.war
     
     # Restore Configuration
-    sudo mv $TMP_PATH/environment.xml_common-metadata-internal_tmp $DEPLOY_TARGET_PATH/common-metadata-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo mv $TMP_PATH/logback.xml_common-metadata-internal_tmp $DEPLOY_TARGET_PATH/common-metadata-internal/$LOGBACK_RELATIVE_PATH_FILE
+    sudo mv $TMP_PATH/environment_internal.xml $DEPLOY_TARGET_PATH/common-metadata-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo mv $TMP_PATH/logback_internal.xml $DEPLOY_TARGET_PATH/common-metadata-internal/$LOGBACK_RELATIVE_PATH_FILE
     
     
     ###
     # COMMON-METADATA
     ###
-    # Backup Configuration
-    sudo mv $DEPLOY_TARGET_PATH/common-metadata/$ENVIRONMENT_RELATIVE_PATH_FILE $TMP_PATH/environment.xml_common-metadata_tmp
-    sudo mv $DEPLOY_TARGET_PATH/common-metadata/$LOGBACK_RELATIVE_PATH_FILE $TMP_PATH/logback.xml_common-metadata_tmp
     
     # Update Process
-    sudo mv common-metadata.war $DEPLOY_TARGET_PATH/common-metadata.war
-    sudo unzip $DEPLOY_TARGET_PATH/common-metadata.war -d $DEPLOY_TARGET_PATH
+    sudo rm -rf $DEPLOY_TARGET_PATH/common-metadata
+    sudo mv $TRANSFER_PATH/common-metadata.war $DEPLOY_TARGET_PATH/common-metadata.war
+    sudo unzip $DEPLOY_TARGET_PATH/common-metadata.war -d $DEPLOY_TARGET_PATH/common-metadata
     sudo rm -rf $DEPLOY_TARGET_PATH/common-metadata.war
     
     # Restore Configuration
-    sudo mv $TMP_PATH/environment.xml_common-metadata_tmp $DEPLOY_TARGET_PATH/common-metadata/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo mv $TMP_PATH/logback.xml_common-metadata_tmp $DEPLOY_TARGET_PATH/common-metadata/$LOGBACK_RELATIVE_PATH_FILE
+    sudo mv $TMP_PATH/environment.xml $DEPLOY_TARGET_PATH/common-metadata/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo mv $TMP_PATH/logback.xml $DEPLOY_TARGET_PATH/common-metadata/$LOGBACK_RELATIVE_PATH_FILE
     
     sudo chown -R metamac.metamac /servers/metamac
     sudo service metamac01 start
