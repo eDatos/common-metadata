1e41b3424b3fd080c7001ba3d569ad0e2aefad02
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2017-07-13 09:27:11 +0100

    METAMAC-2628 - Forzar el encoding de las aplicaciones externas > Autoformatting

--- metamac-common-metadata-core/src/test/java/org/siemac/metamac/common/metadata/core/serviceapi/CommonMetadataBaseTests.java
+++ metamac-common-metadata-core/src/test/java/org/siemac/metamac/common/metadata/core/serviceapi/CommonMetadataBaseTests.java
@@ -17,16 +17,15 @@ import org.springframework.beans.factory.annotation.Value;
 
 public abstract class CommonMetadataBaseTests extends MetamacDBUnitBaseTests {
 
-    protected static Long   NOT_EXISTS_ID                                = Long.valueOf(-1);
-    protected static String NOT_EXISTS_CODE                              = "NOT_EXISTS";
+    protected static Long   NOT_EXISTS_ID                                               = Long.valueOf(-1);
+    protected static String NOT_EXISTS_CODE                                             = "NOT_EXISTS";
 
-    protected static Long   DATA_CONFIGURATION_01_ID                     = Long.valueOf(1);
-    protected static String DATA_CONFIGURATION_01_KEY                    = "metamac.common_metadata.db.url";
-    protected static int    NUMBER_DATA_CONFIGURATIONS_SYSTEM_PROPERTIES = 38;
-    protected static int    NUMBER_DATA_CONFIGURATIONS_DEFAULT_VALUES    = 5;
+    protected static Long   DATA_CONFIGURATION_01_ID                                    = Long.valueOf(1);
+    protected static String DATA_CONFIGURATION_01_KEY                                   = "metamac.common_metadata.db.url";
+    protected static int    NUMBER_DATA_CONFIGURATIONS_SYSTEM_PROPERTIES                = 38;
+    protected static int    NUMBER_DATA_CONFIGURATIONS_DEFAULT_VALUES                   = 5;
     protected static int    NUMBER_DATA_CONFIGURATIONS_SYSTEM_PROPERTIES_WITH_CODE_SDMX = 4;
     protected static int    NUMBER_DATA_CONFIGURATIONS_DEFAULT_VALUES_WITH_CODE_SDMX    = 4;
-    
 
     @Value("${metamac.common_metadata.db.provider}")
     private String          databaseProvider;
