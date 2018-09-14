b5fc23372d233041bd1ff87d2e414866b0455204
Javier Villamonte <jvilper@arte-consultores.com>
2018-09-14 13:56:52 +0100

    METAMAC-2837 - Limpiar de la aplicación las cuestiones relativas a CAS y e-mail
    >Se dan de alta propiedades nuevas.
    >Se suben las versiones de metamac-core-common y metamac-sso.

---
+++ etc/changes-from-release/2.2.0/db/common-metadata/mssql/20180914_insert-sie-header-footer-url-configuration.sql
@@ -0,0 +1,11 @@
+﻿-- --------------------------------------------------------------------------------------------------
+-- METAMAC-2837 - Limpiar de la aplicación las cuestiones relativas a CAS y e-mail
+-- --------------------------------------------------------------------------------------------------
+
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values (FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'),1,1,'sie.style.header.url','FILL_ME',1);
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values (FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'),1,1,'sie.style.footer.url','FILL_ME',1);
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+commit;
