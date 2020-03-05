9f700bfbb924945c1e0d770c3fa605b6a7805b6e
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-03-05 08:12:04 +0000

    EDATOS-3123 - Integración de la aplicación en GITLAB CI para la compilación y despliegue de la aplicación
    > Se actualiza el número de propiedades después de haber añadido las necesarias para la aplicación statistical-operations-roadmap

--- metamac-common-metadata-core/src/test/java/org/siemac/metamac/common/metadata/core/serviceapi/CommonMetadataBaseTests.java
+++ metamac-common-metadata-core/src/test/java/org/siemac/metamac/common/metadata/core/serviceapi/CommonMetadataBaseTests.java
@@ -22,7 +22,7 @@ public abstract class CommonMetadataBaseTests extends MetamacDBUnitBaseTests {
 
     protected static Long   DATA_CONFIGURATION_01_ID                                    = Long.valueOf(1);
     protected static String DATA_CONFIGURATION_01_KEY                                   = "metamac.common_metadata.db.url";
-    protected static int    NUMBER_DATA_CONFIGURATIONS_SYSTEM_PROPERTIES                = 53;
+    protected static int    NUMBER_DATA_CONFIGURATIONS_SYSTEM_PROPERTIES                = 57;
     protected static int    NUMBER_DATA_CONFIGURATIONS_DEFAULT_VALUES                   = 5;
     protected static int    NUMBER_DATA_CONFIGURATIONS_SYSTEM_PROPERTIES_WITH_CODE_SDMX = 4;
     protected static int    NUMBER_DATA_CONFIGURATIONS_DEFAULT_VALUES_WITH_CODE_SDMX    = 4;
