05afed743731e6cd55377e9320094068474e5cca
Miguel Jonás Bravo García <mbragar@arte-consultores.com>
2020-04-01 13:19:19 +0100

    EDATOS-3146 - Instalación del entorno de estadísticas en la nueva máquina OVH
    > Modificaciones en el fichero deploy.sh propuesta durante la revisión del merge request

--- etc/deploy/deploy.sh
+++ etc/deploy/deploy.sh
@@ -2,7 +2,7 @@
 
 HOME_PATH=metamac-common-metadata
 TRANSFER_PATH=$HOME_PATH/tmp
-DEPLOY_TARGET_PATH=/servers/edatos-external/tomcats/edatos-external01/webapps
+DEPLOY_TARGET_PATH_EXTERNAL=/servers/edatos-external/tomcats/edatos-external01/webapps
 DEPLOY_TARGET_PATH_INTERNAL=/servers/edatos-internal/tomcats/edatos-internal01/webapps
 ENVIRONMENT_RELATIVE_PATH_FILE=WEB-INF/classes/metamac/environment.xml
 LOGBACK_RELATIVE_PATH_FILE=WEB-INF/classes/logback.xml
@@ -12,7 +12,6 @@ if [ "$1" == "--no-restart" ]; then
     RESTART=0
 fi
 
-
 scp -r etc/deploy deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH
 scp metamac-common-metadata-web/target/common-metadata-internal-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/common-metadata-internal.war
 scp metamac-common-metadata-external-web/target/common-metadata-*.war deploy@estadisticas.arte-consultores.com:$TRANSFER_PATH/common-metadata.war
@@ -21,16 +20,14 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     chmod a+x $TRANSFER_PATH/deploy/*.sh;
     . $TRANSFER_PATH/deploy/utilities.sh
 
-    if [ $RESTART -eq 1 ]; then
-        sudo service edatos-internal01 stop
-        sudo service edatos-external01 stop
-        checkPROC "edatos-internal"
-        checkPROC "edatos-external"
-    fi
-
     ###
     # COMMON-METADATA-INTERNAL
     ###
+    
+    if [ $RESTART -eq 1 ]; then
+        sudo service edatos-internal01 stop
+        checkPROC "edatos-internal"
+    fi
 
     # Update Process
     sudo rm -rf $DEPLOY_TARGET_PATH_INTERNAL/common-metadata-internal
@@ -41,31 +38,36 @@ ssh deploy@estadisticas.arte-consultores.com <<EOF
     # Restore Configuration
     sudo cp $HOME_PATH/environment_internal.xml $DEPLOY_TARGET_PATH_INTERNAL/common-metadata-internal/$ENVIRONMENT_RELATIVE_PATH_FILE
     sudo cp $HOME_PATH/logback_internal.xml $DEPLOY_TARGET_PATH_INTERNAL/common-metadata-internal/$LOGBACK_RELATIVE_PATH_FILE
+    
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R edatos-internal.edatos-internal /servers/edatos-internal     
+        sudo service edatos-internal01 start
+    fi
 
 
     ###
     # COMMON-METADATA
     ###
-
-    # Update Process
-    sudo rm -rf $DEPLOY_TARGET_PATH/common-metadata
-    sudo mv $TRANSFER_PATH/common-metadata.war $DEPLOY_TARGET_PATH/common-metadata.war
-    sudo unzip $DEPLOY_TARGET_PATH/common-metadata.war -d $DEPLOY_TARGET_PATH/common-metadata
-    sudo rm -rf $DEPLOY_TARGET_PATH/common-metadata.war
-
-    # Restore Configuration
-    sudo cp $HOME_PATH/environment.xml $DEPLOY_TARGET_PATH/common-metadata/$ENVIRONMENT_RELATIVE_PATH_FILE
-    sudo cp $HOME_PATH/logback.xml $DEPLOY_TARGET_PATH/common-metadata/$LOGBACK_RELATIVE_PATH_FILE
-
+    
     if [ $RESTART -eq 1 ]; then
-        sudo chown -R edatos-internal.edatos-internal /servers/edatos-internal
-        sudo chown -R edatos-external.edatos-external /servers/edatos-external        
-        sudo service edatos-internal01 start
-        sudo service edatos-external01 start
+        sudo service edatos-external01 stop
+        checkPROC "edatos-external"
     fi
 
-    #checkURL "http://estadisticas.arte-consultores.com/common-metadata-internal" "metamac01"
-    #checkURL "http://estadisticas.arte-consultores.com/cmetadata/latest" "metamac01"
+    # Update Process
+    sudo rm -rf $DEPLOY_TARGET_PATH_EXTERNAL/common-metadata
+    sudo mv $TRANSFER_PATH/common-metadata.war $DEPLOY_TARGET_PATH_EXTERNAL/common-metadata.war
+    sudo unzip $DEPLOY_TARGET_PATH_EXTERNAL/common-metadata.war -d $DEPLOY_TARGET_PATH_EXTERNAL/common-metadata
+    sudo rm -rf $DEPLOY_TARGET_PATH_EXTERNAL/common-metadata.war
+
+    # Restore Configuration
+    sudo cp $HOME_PATH/environment_external.xml $DEPLOY_TARGET_PATH_EXTERNAL/common-metadata/$ENVIRONMENT_RELATIVE_PATH_FILE
+    sudo cp $HOME_PATH/logback_external.xml $DEPLOY_TARGET_PATH_EXTERNAL/common-metadata/$LOGBACK_RELATIVE_PATH_FILE
+
+    if [ $RESTART -eq 1 ]; then
+        sudo chown -R edatos-external.edatos-external /servers/edatos-external        
+        sudo service edatos-external01 start
+    fi
 
     echo "Finished deploy"
 EOF
