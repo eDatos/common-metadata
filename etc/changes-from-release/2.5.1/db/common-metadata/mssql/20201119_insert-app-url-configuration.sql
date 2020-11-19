b4d2484523c7bbc2b9ed314bb4529f79f96dba03
Francisco Javier Rodríguez García <frodgar@arte-consultores.com>
2020-11-19 11:52:39 +0000

    EDATOS-3215 - Refactorización del código para que la propiedad organización sea escalable
    > Nuevas propiedades para desvincular ciertas logicas como el favicon y el addthis de la organización

---
+++ etc/changes-from-release/2.5.1/db/common-metadata/mssql/20201119_insert-app-url-configuration.sql
@@ -0,0 +1,25 @@
+﻿-- --------------------------------------------------------------------------------------------------
+-- EDATOS-3215 - Refactorización del código para que la propiedad organización sea escalable
+-- --------------------------------------------------------------------------------------------------
+
+-- Raíz de complementos apps (external)
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.apps.web.external.url', 'FILL_ME', 1);
+-- Ejemplo PRO: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.apps.web.external.url', 'https://www3.gobiernodecanarias.org/aplicaciones/appsistac', 1);
+-- Ejemplo PRE: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.apps.web.external.url', 'https://www3-pre.gobiernodecanarias.org/aplicaciones/appsistac', 1);
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+-- Raíz de complementos apps (internal)
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.apps.web.internal.url', 'FILL_ME', 1);
+-- Ejemplo PRO: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.apps.web.internal.url', 'https://www3.gobiernodecanarias.net/aplicaciones/appsinternalistac', 1);
+-- Ejemplo PRE: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.apps.web.internal.url', 'https://www3-pre.gobiernodecanarias.net/aplicaciones/appsinternalistac/', 1);
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+-- Código para las analíticas de AddThis
+insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.analytics.addthis.code', '', 1);
+-- Ejemplo PRO: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.analytics.addthis.code', 'ra-501fc6f600bacbe9', 1);
+-- Ejemplo PRE: insert into TB_DATA_CONFIGURATIONS (ID,VERSION,SYSTEM_PROPERTY,CONF_KEY,CONF_VALUE,EXTERNALLY_PUBLISHED) values(FILL_WITH_SCHEMA_NAME.GetSequenceNextValue('DATA_CONFIGURATIONS'), 1, 1, 'metamac.analytics.addthis.code', 'ra-501fc6f600bacbe9', 1);
+
+UPDATE TB_SEQUENCES SET SEQUENCE_NEXT_VALUE = SEQUENCE_NEXT_VALUE + 1 WHERE SEQUENCE_NAME = 'DATA_CONFIGURATIONS';
+
+commit;
+
